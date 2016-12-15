8e5e83aa9ea37d8eaf4496706d4057400481c106
Sergio Trujillo Martínez <sergiotrujillomartinez@gmail.com>
2016-12-15 01:30:33 +0100

    Add model

---
+++ model/user/user.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#    TuiterPy - A Python Command Line Social Network Application
+#    Copyright (C) 2016  Sergio Trujillo (sergiotrujillomartinez@gmail.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+
+class User(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.followings = []
+
+    def add_follow(self, followed):
+        self.followings.append(followed)
+
+    def __hash__(self):
+        return super(User, self).__hash__()
+
+    def __eq__(self, other):
+        if other is self:
+            return True
+        if other is None or self.__class__ != other.__class__:
+            return False
+        return self.__dict__ == other.__dict__
+
+    def __hash__(self):
+        return hash(tuple(sorted(self.__dict__.items())))
---
+++ model/user/user_repository.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#    TuiterPy - A Python Command Line Social Network Application
+#    Copyright (C) 2016  Sergio Trujillo (sergiotrujillomartinez@gmail.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+
+class UserRepository(object):
+
+    def get(self, user_name):
+        return NotImplementedError
+
+    def create(self, user):
+        return NotImplementedError
+
+    def follow(self, follower, followed):
+        return NotImplementedError
+
+    def get_post_by_user(self, user):
+        return NotImplementedError
+
+    def add(self, post):
+        return NotImplementedError
